5ee06dd2bcfb78f67a3124ce57b46ca80d4123b8
Reza Adamir <stu238357@mail.uni-kiel.de>
2026-03-24 19:53:44 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from app.database import get_connection
+from app.api.patients import router as patient_router
 
 app = FastAPI()
 
+app.include_router(patient_router)
+
+
+"""
 @app.get("/")
 def root():
     conn = get_connection()
     conn.close()
     return {"message": "DB kliniken Connection ist da...."}
 
-"""
+
 def main():
     print("Kliniken Backend startet...")
     # Hier später FastAPI / Flask App starten
